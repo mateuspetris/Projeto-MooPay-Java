caba7a332fff66c33ccd7e1550dc359e13d0dbc4
Mateus Petris <mateuspetris@gmail.com>
2026-01-16 16:43:47 -0300

    Implement PagamentoCartao class for card transactions

---
+++ PagamentoCartao.java
@@ -0,0 +1,25 @@
+package Projetos;
+
+public class PagamentoCartao extends Transacao {
+
+    private String numero;
+
+    public PagamentoCartao(){
+
+    }
+
+    public PagamentoCartao(int id, double valor, StatusTransacao status, String numero){
+        super(id,valor,status);
+        this.numero = numero;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    @Override
+    public void processarPagamento() {
+        System.out.println("Pagamento de R$" + getValor() + " Cartão final " + numero.substring(numero.length()-4));
+        this.setStatus(StatusTransacao.APROVADO);
+    }
+}
